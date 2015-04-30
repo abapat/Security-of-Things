0f8747385e8a8484e6e24c7dd9c5cca2da62109c
sweet potatoe <allison.ng@stonybrook.edu>
2015-04-29 20:05:08 -0400

    hashed passwords in server.py and client.py

--- client.py
+++ client.py
@@ -1,15 +1,26 @@
 import socket
 import sys
+import uuid
+import hashlib
 
-# Create a UDP socket
+def hash_password(password, salt):
+
+	hashedpassword = hashlib.sha256(password.encode()).hexdigest() 
+	return hashlib.sha256(salt.encode() + hashedpassword.encode()).hexdigest()
+
+#create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-server_address = ('', 5000)
+server_address = ('', 50000)
 sock.bind(server_address)
 
-while True: 
-	print 'ready!'
-	data, server = sock.recvfrom(4096)
-	print data
+while True:
+	# Receive response
+	salt, server = sock.recvfrom(4096)
+
+	password = raw_input("password : ")
+	hashed = hash_password(password, salt)
+	print hashed
+
+	sock.sendto(hashed, server)
 
 sock.close()
