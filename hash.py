0d940d98cb1548e0ffb50836486d016a35842cf7
Allison Ng <allison.ng@stonybrook.edu>
2016-09-22 22:04:15 -0400

    save user and password information in a mongodb database rather than in a jank file

--- hash.py
+++ hash.py
@@ -1,5 +1,7 @@
 import os
 import hashlib
+from pymongo import MongoClient
+
 '''
 f = open('passwords', 'w')
 line = []
@@ -16,11 +18,30 @@ for field in line:
 
 f.close()
 '''
+'''
+#writes default username and password to file
 f = open('passwords', 'w')
 f.write("admin,")
 m = hashlib.sha256()
 m.update("pass")
 f.write(m.hexdigest())
 f.close()
+'''
+def add_user(username, password):
+  client = MongoClient()
+  db = client.IOT
 
+  #TODO:sanitize data
 
+  #check to see if that username is already used
+  if db.login_info.find_one({"username": username}) is None:
+    #hash password
+    m = hashlib.sha256()
+    m.update(password)
+    db.login_info.insert_one({"username": username, "password": m.hexdigest()})
+  else:
+    print "Error: Username \""+username+"\" is already in use."
+
+username = raw_input("username : ")
+password = raw_input("password : ")
+add_user(username, password)
